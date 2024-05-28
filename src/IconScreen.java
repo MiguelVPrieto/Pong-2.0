8bf483c6066b3806685b056fc1d67cc37446352f
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-05-28 20:48:48 -0300

    main

--- src/IconScreen.java
+++ src/IconScreen.java
@@ -3,8 +3,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.Image;
-import java.awt.Toolkit;
 
 public class IconScreen {
     public static void main(String[] args) {
@@ -16,10 +14,6 @@ public class IconScreen {
             System.err.println("Error loading icon file.");
             return;
         }
-
-        // Scale the image to be twice as large
-        Image scaledBlock = Bl_icon.getImage().getScaledInstance(Bl_icon.getIconWidth() * 2, Bl_icon.getIconHeight() * 2, Image.SCALE_DEFAULT);
-        Bl_icon = new ImageIcon(scaledBlock);
 
         JLabel block = new JLabel(Bl_icon);
         block.setBounds(910, 750, Bl_icon.getIconWidth(), Bl_icon.getIconHeight()); // set bounds instead of location
@@ -38,14 +32,6 @@ public class IconScreen {
                         x += 10;
                         break;
                 }
-
-                // Check if the block is still within the visible area of the frame
-                if (x < 0) {
-                    x = 0;
-                } else if (x + Bl_icon.getIconWidth() > frame.getWidth()) {
-                    x = frame.getWidth() - Bl_icon.getIconWidth();
-                }
-
                 block.setBounds(x, y, Bl_icon.getIconWidth(), Bl_icon.getIconHeight()); // update bounds
                 System.out.println("x: " + x + ", y: " + y);
             }
