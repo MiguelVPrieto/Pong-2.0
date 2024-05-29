be230c99bd1079fcc7295312a6b82247bf5c6e90
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-05-29 18:00:45 -0300

    full screen + fixed block position + fixed movements

--- src/IconScreen.java
+++ src/IconScreen.java
@@ -8,6 +8,9 @@ import java.awt.event.KeyEvent;
 public class IconScreen {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Pong-2.0");
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+
         String filePath = "src/block.png"; // replace with your absolute path
         ImageIcon Bl_icon = new ImageIcon(filePath);
 
@@ -19,44 +22,48 @@ public class IconScreen {
         // Set the size of the icon
         Bl_icon.setImage(Bl_icon.getImage().getScaledInstance(300, 150, Image.SCALE_SMOOTH));
         block.setIcon(Bl_icon);
-        block.setBounds(830, 750, 300, 150); // set bounds instead of location
+
+        // Set the initial position of the block to be in the center of the screen
+        int x = (int) (screenSize.getWidth() - 300) / 2;
+        int y = (int) (screenSize.getHeight() * 0.9 - 150);w    a
+        block.setBounds(x, y, 300, 150); // set bounds instead of location
 
         frame.addKeyListener(new KeyAdapter() {
-            int x = 830; // adjust the x position to fit the label
-            int y = 750;
+            int xTemp = x; // adjust the x position to fit the label
+            int yTemp = y;
             @Override
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_A:
-                        x -= 10;
+                        xTemp -= 10;
                         break;
                     case KeyEvent.VK_D:
-                        x += 10;
+                        xTemp += 10;
                         break;
                 }
 
                 // Check if the block is still within the visible area of the frame
-                if (x < 0) {
-                    x = 0;
-                } else if (x + 800 > frame.getWidth()) {
-                    x = frame.getWidth() - 300;
+                if (xTemp < 0) {
+                    xTemp = 0;
+                } else if (xTemp + 300 > frame.getWidth()) {
+                    xTemp = frame.getWidth() - 300;
                 }
 
-                if (y < 0) {
-                    y = 0;
-                } else if (y + 40 > frame.getHeight()) {
-                    y = frame.getHeight() - 150;
+                if (yTemp < 0) {
+                    yTemp = 0;
+                } else if (yTemp + 150 > frame.getHeight()) {
+                    yTemp = frame.getHeight() - 150;
                 }
 
-                block.setBounds(x, y, 300, 150); // update bounds
-                System.out.println("x: " + x + ", y: " + y);
+                block.setBounds(xTemp, yTemp, 300, 150); // update bounds
+                System.out.println("x: " + xTemp + ", y: " + yTemp);
             }
         });
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null); // set layout to null
         frame.getContentPane().add(block);
-        frame.setSize(1200, 900); // set size of frame to be larger than the block's position
+        frame.setSize(screenSize.width, screenSize.height); // set size of frame to be the same as the screen size
         frame.setVisible(true);
 
         // Move the focus to the frame so that key events are received
